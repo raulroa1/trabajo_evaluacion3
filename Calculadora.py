3ff363c1cd62cb52c4e1bae48fd291f2dc9bfe01
Raul Roa <ra.roa@duocuc.cl>
2023-06-14 20:37:18 -0400

    Guardado definitivo

---
+++ Calculadora.py
@@ -0,0 +1,50 @@
+menu=True
+opcion1=0
+
+def menu1():
+    print("----------------BIENVENIDO A LA CALCULADORA DE ROA---------------\n1.-Suma\n2.-Resta\n3.-Multiplicar\n4.-Dividir\n5.-Salir")
+def suma1():
+    resultado=num_1+num_2
+    return resultado
+def resta1():
+    resultado=num_1-num_2
+    return resultado
+def multiplicacion1():
+    resultado=num_1*num_2
+    return resultado
+def division1():
+    resultado=num_1/num_2
+    return resultado
+def menu2():
+    print("----------------LA CALCULADORA DE ROA---------------")
+    
+while menu:
+    menu1()
+    opcion1=int(input("Ingrese una opcion"))
+    if opcion1==1:
+        menu2()
+        print("Has seleccionado la opcion 1.-Suma")
+        num_1=int(input("INGRESE UN NUMERO PARA SUMAR:"))
+        num_2=int(input("INGRESE EL SEGUNDO NUMERO A SUMAR:"))
+        print("El resultado de la suma es:",suma1())
+    elif opcion1==2:
+        menu2()
+        print("Has seleccionado la opcion 2.-Resta")
+        num_1=int(input("INGRESE UN NUMERO PARA RESTAR:"))
+        num_2=int(input("INGRESE EL SEGUNDO NUMERO A RESTAR:"))
+        print("El resultado de la resta es:",resta1())
+    elif opcion1==3:
+        menu2()
+        print("Has seleccionado la opcion 3.-Multiplicacion")
+        num_1=int(input("INGRESE UN NUMERO PARA MULTIPLICAR:"))
+        num_2=int(input("INGRESE EL SEGUNDO NUMERO A MULTIPLICAR:"))
+        print("El resultado de la multiplicacion es:",multiplicacion1())
+    elif opcion1==4:
+        menu2()
+        print("Has seleccionado la opcion 4.-Dividir")
+        num_1=int(input("INGRESE UN NUMERO PARA DIVIDIR:"))
+        num_2=int(input("INGRESE EL SEGUNDO NUMERO A DIVIDIR:"))
+        print("El resultado de la division es:",division1())
+    else:
+        print("Has salido del menu")
+        menu=False
--- nueva unidad.py
+++ nueva unidad.py
@@ -1,3 +1,31 @@
+#sin argumentos y sin retorno
 def saludo ():
-    print("saludando a mis estuadiantes")
+    print("saludando a mis estudiantes")
 saludo()
+saludo()
+saludo()
+#sin argumentos con retorno
+def suma1():
+    num_1=10
+    num_2=5
+    resultado=num_1+num_2
+    return resultado
+resultado_suma=suma1()
+print("Forma A de utilizar funcion con retorno:",resultado_suma)
+print("Forma B de utilizar funcion con retorno:",suma1())
+#funcion con argumento y sin retorno
+def resta1(num_1,num_2):
+    resultado=num_1-num_2
+    print("El resultado de la resta es:", resultado )
+resta1(10,1)
+
+#funcion con argumento y con retorno
+def resta2(num_1,num_2):
+    resultado=num_1-num_2
+    return resultado
+resultado_1=resta2(10,1)#9
+resultado_2=resta2(8,3)#5
+#forma decente
+print(resultado_1-resultado_2)#4
+#forma bizarra
+print(resta2(10,1)+resta2(8,3))#14
